3aa8f56d1e9f41bf13eaa3f96d875cd7d35880e7
demattei-teo <teitodematteio@gmail.com>
2023-10-24 18:22:23 -0300

    feat: add header to page

--- src/app/[language]/page.tsx
+++ src/app/[language]/page.tsx
@@ -1,11 +1,12 @@
+import { Header } from '@/components'
 import { getDictionary } from '@/get-dictionary'
 import { Locale } from '@/i18n-config'
-
 export default async function Home({ params: { language } }: { params: { language: Locale } }) {
   const Dictionary = await getDictionary(language)
 
   return (
     <>
+      <Header />
       <main></main>
     </>
   )
