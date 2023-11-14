341225788e966be8b1351ec4667f6477782436f6
demattei-teo <teitodematteio@gmail.com>
2023-11-14 12:14:58 -0300

    feat(ui): add portfolio interface

--- src/app/[language]/page.tsx
+++ src/app/[language]/page.tsx
@@ -1,4 +1,4 @@
-import { Header, Hero } from '@/components'
+import { Header, Hero, Portfolio, PortfolioCard } from '@/components'
 import { getDictionary } from '@/get-dictionary'
 import { Locale } from '@/i18n-config'
 export default async function Home({ params: { language } }: { params: { language: Locale } }) {
@@ -9,6 +9,12 @@ export default async function Home({ params: { language } }: { params: { languag
       <Header />
       <main>
         <Hero />
+        <Portfolio>
+          <PortfolioCard srcImg='/assets/mockups/lawyer.jpg' srcLink='#' altImg='lawyer mockup' />
+          <PortfolioCard srcImg='/assets/mockups/lottodo-mock2.jpg' srcLink='#' altImg='lottodo mockup' />
+          <PortfolioCard srcImg='/assets/mockups/mova.jpeg' srcLink='#' altImg='mova mockup' />
+          <PortfolioCard srcImg='/assets/mockups/rodaroArquitectos.jpg' srcLink='#' altImg='rodaro arquitectos mockup' />
+        </Portfolio>
       </main>
     </>
   )
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,12 +1,12 @@
-import { fotoCV } from '@/assets'
 import Image from 'next/image'
 import { InfoCard } from '..'
+import { fotoCV } from '../../../public/assets'
 
 function Hero() {
   return (
-    <section className='grid gap-5 sm:grid-cols-3 p-4 wrapper items-center'>
+    <section className='grid gap-5 sm:grid-cols-3 wrapper items-center'>
       <div className='relative h-80 sm:col-start-3 sm:col-end-4 sm:h-full w-full'>
-        <Image className='object-cover rounded-lg shadow-sm' fill src={fotoCV} alt='Teo Demattei' />
+        <Image sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw' className='object-cover rounded-2xl shadow-sm' priority fill src={fotoCV} alt='Teo Demattei' />
       </div>
       <InfoCard title_h1='Hola, Soy Teo Demattei' description='Soy desarrollador web frontend hace mas de dos años, tuve la oportunidade de trabajar en Nudebi y en No Country' />
     </section>
