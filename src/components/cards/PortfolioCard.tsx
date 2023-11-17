df94ea59c658f3dd16b26a414f3ae02c09d1f781
demattei-teo <teitodematteio@gmail.com>
2023-11-17 13:02:51 -0300

    feat(ui): add shapes to interfaces

--- src/components/cards/PortfolioCard.tsx
+++ src/components/cards/PortfolioCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export default function PortfolioCard({ srcImg, altImg, srcLink }: Props) {
   return (
-    <Link href={srcLink} className='z-20 relative min-h-[350px] group'>
+    <Link href={srcLink} className='shadow-md z-20 rounded-2xl relative min-h-[350px] group'>
       <div
         className='absolute right-5 bottom-5 transition lg:opacity-0
        rounded-full shadow-sm lg:group-hover:opacity-100 z-20'
@@ -18,7 +18,7 @@ export default function PortfolioCard({ srcImg, altImg, srcLink }: Props) {
         <IconLink />
       </div>
       <div className='absolute w-full h-full transition-opacity ease-linear duration-300 bg-black lg:opacity-0 lg:group-hover:opacity-40 rounded-2xl opacity-40' />
-      <Image fill src={srcImg} alt={altImg} sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw' className='rounded-2xl -z-10 md:object-fill' />
+      <Image fill src={srcImg} alt={altImg} sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw' className='rounded-2xl -z-10 object-cover' />
     </Link>
   )
 }
--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,7 +1,14 @@
+import Image from 'next/image'
+
 interface Props {
   children: React.ReactNode
 }
 
 export default function Portfolio({ children }: Props) {
-  return <section className='wrapper grid grid-cols-1 md:grid-cols-2 min-h-screen gap-5'>{children}</section>
+  return (
+    <section className='wrapper relative grid grid-cols-1 md:grid-cols-2 min-h-screen gap-5'>
+      <Image width={150} height={150} src='/assets/shapes/shape4.png' className='absolute bottom-[40%] -left-16 animate-spin animate-infinite animate-duration-[80s] animate-ease-linear' alt='shape' sizes='33vw' />
+      {children}
+    </section>
+  )
 }
