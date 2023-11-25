1ce8f008a2522500c50d2bd98927f2cef613d342
demattei-teo <teitodematteio@gmail.com>
2023-11-24 22:42:30 -0300

    perf: add metadata

--- src/app/[language]/[proyects]/components/wrapper/WrapperInfoProyect.tsx
+++ src/app/[language]/[proyects]/components/wrapper/WrapperInfoProyect.tsx
@@ -7,15 +7,17 @@ interface WrapperInfoProyectProps {
   title_h1: InfoCardProps['title_h1']
   description: InfoCardProps['description']
   tecnologies: InfoCardProps['tecnologies']
+  linkProyect?: InfoCardProps['LinkProyect']
+  githubUrlProyect?: InfoCardProps['githubUrlProyect']
 }
 
-export default function WrapperInfoProyect({ srcImg, title_h1, description, tecnologies }: WrapperInfoProyectProps) {
+export default function WrapperInfoProyect({ srcImg, title_h1, githubUrlProyect, description, linkProyect, tecnologies }: WrapperInfoProyectProps) {
   return (
     <>
       <div className='relative h-80 lg:h-full w-full shadow-md rounded-2xl'>
         <Image src={srcImg} priority fill alt='lawyer mockup' sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw' className='object-cover rounded-2xl' />
       </div>
-      <InfoCard mode='proyect' title_h1={title_h1} description={description} tecnologies={tecnologies} />
+      <InfoCard mode='proyect' title_h1={title_h1} description={description} tecnologies={tecnologies} LinkProyect={linkProyect} githubUrlProyect={githubUrlProyect} />
     </>
   )
 }
